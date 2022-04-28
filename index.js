04234e7b3160f65b38b75d24bd438bf5a3ab59e5
ja10ns0 <alonsofernandezjaime@gmail.com>
2022-04-29 00:08:57 +0200

    Initial commit and setup

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 // Initialize and add the map
 function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.031 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: uluru,
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: uluru,
+  // The location of Jakarta
+  const arrLocations1 = [
+    { 
+      name: 'Jakarta',
+      lat: -6.19138433712882, 
+      lng: 106.8440013947889 
+    },
+    {
+      name: 'Yogjakarta',
+      lat: -7.7956781393307955,
+      lng: 110.37015555608066
+    },
+    {
+      name: 'Ratu Boko',
+      lat: -7.744917827384563,
+      lng: 110.48291270045232
+    }
+  ];
+  const arrLocations2 = [
+    { 
+      name: 'Borneo',
+      lat: -2.7309635230030707, 
+      lng: 111.73217292223129
+    }
+  ];
+
+  var markers = [];
+
+  // The map, centered at Jakarta
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4
+  });
+
+  const bounds = new google.maps.LatLngBounds();
+
+  arrLocations1.forEach(location => {
+    let marker = new google.maps.Marker({
+      position: location,
       map: map,
+      title: location.name
     });
-  }
-  
-  window.initMap = initMap;
+    markers.push(marker);
+    bounds.extend(location);
+  });
+
+  map.fitBounds(bounds);
+  map.panToBounds(bounds);
+
+  // Bind event listener on button to reload markers
+  document.getElementById('changeDay').addEventListener('click', reloadMarkers);
+
+}
+
+window.initMap = initMap;
