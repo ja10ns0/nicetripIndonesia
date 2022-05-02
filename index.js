5dff0aba5fb5ba0d9ab58d6f45c518ba7e7f8dc9
ja10ns0 <alonsofernandezjaime@gmail.com>
2022-05-02 08:50:22 +0200

    Basic script and layout

--- index.js
+++ index.js
@@ -1,56 +1,87 @@
-// Initialize and add the map
-function initMap() {
-  // The location of Jakarta
-  const arrLocations1 = [
-    { 
-      name: 'Jakarta',
-      lat: -6.19138433712882, 
-      lng: 106.8440013947889 
-    },
-    {
-      name: 'Yogjakarta',
-      lat: -7.7956781393307955,
-      lng: 110.37015555608066
-    },
-    {
-      name: 'Ratu Boko',
-      lat: -7.744917827384563,
-      lng: 110.48291270045232
-    }
-  ];
-  const arrLocations2 = [
-    { 
-      name: 'Borneo',
-      lat: -2.7309635230030707, 
-      lng: 111.73217292223129
-    }
-  ];
 
-  var markers = [];
+import data from "./data.json" assert { type: "json" };
 
-  // The map, centered at Jakarta
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4
-  });
+var arrDay = JSON.parse(JSON.stringify(data));
 
-  const bounds = new google.maps.LatLngBounds();
+var map;
 
-  arrLocations1.forEach(location => {
+var markers = [];
+
+var selectedDay = 0;
+
+function setMarkers(day) {
+  
+  var bounds = new google.maps.LatLngBounds;
+
+  day['locations'].forEach(location => {
     let marker = new google.maps.Marker({
       position: location,
       map: map,
       title: location.name
     });
+    bounds.extend(new google.maps.LatLng(location));    
     markers.push(marker);
-    bounds.extend(location);
   });
 
   map.fitBounds(bounds);
-  map.panToBounds(bounds);
 
-  // Bind event listener on button to reload markers
-  document.getElementById('changeDay').addEventListener('click', reloadMarkers);
+  // Update info (day)
+  var infoDate = document.querySelector('#date');
+  infoDate.innerHTML = "Día " + day['order'] + " - " + day['date'];
+  // Update info (planning)
+  // Cache of the template
+  var template = document.getElementById("template-info-planning");
+  // Get the contents of the template
+  var templateHtml = template.innerHTML;
+  // Final HTML variable as empty string
+  var infoPlanning = "";
+  // Loop through dataObject
+  day['locations'].forEach(function (location, index) {
+    infoPlanning += templateHtml.replace(/{{order}}/g, index + 1)
+                                .replace(/{{label}}/g, location["label"]);
+  });
+  // Replace the HTML of #list with final HTML
+  document.getElementById("info__planning").innerHTML = infoPlanning;
+}
+
+function moveDay(param) {
+
+  console.log('markers', markers);
+  // Loop through markers and set map to null for each
+  for (var i=0; i<markers.length; i++) {
+      markers[i].setMap(null);
+  }
+  
+  // Reset the markers array
+  markers = [];
+  console.log('selected day', selectedDay);
+  selectedDay = selectedDay + param;
+  console.log('selected day mow', selectedDay);
+  
+  // Call set markers to re-add markers
+  setMarkers(arrDay[selectedDay]);
+  
+}
+
+function initMap() {
+
+  // Initilize map centered at Jakarta
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: arrDay[0]['locations'][0],
+    zoom: 6,
+  });
+
+  // Set markers for day 1
+  setMarkers(arrDay[selectedDay]);
 
 }
+
+// Bind event listener on button to reload markers
+document.getElementById('next').addEventListener('click', function(){
+  moveDay(1);
+});
+document.getElementById('prev').addEventListener('click', function(){
+  moveDay(-1);
+});
 
 window.initMap = initMap;
